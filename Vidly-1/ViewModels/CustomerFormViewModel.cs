2dc8426803bf49a736cd89be5c5f9baaeafda4f5
Dario Vranjković <vranja33@gmail.com>
2017-02-24 17:54:13 +0100

    Add Movies content

---
+++ Vidly-1/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly_1.Models;
+
+namespace Vidly_1.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customers { get; set; }
+    }
+}
