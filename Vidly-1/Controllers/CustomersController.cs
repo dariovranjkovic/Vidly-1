fc19c68fcb18c37d3c0f3f66b7fa15d07755a83f
Dario Vranjković <vranja33@gmail.com>
2017-02-25 20:24:05 +0100

    Model binding - New customer

--- Vidly-1/Controllers/CustomersController.cs
+++ Vidly-1/Controllers/CustomersController.cs
@@ -33,6 +33,12 @@ namespace Vidly_1.Controllers
             return View(viewModel);
         }
 
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            return View();
+        }
+
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
